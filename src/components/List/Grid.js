7f16f08fdc75ad83c39cb1289bcb8e3e0a1d59a6
daniel.velazquez <danicond97@gmail.com>
2022-01-15 15:30:47 -0600

    fix: eslint fix

--- src/components/List/Grid.js
+++ src/components/List/Grid.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import './Grid.styles.css'
+import React from 'react';
+import './Grid.styles.css';
 
 import Single from './Single';
 
 function Grid({ items }) {
-    const renderList = () => {
-        return items.map((item, index) => (
-          <Single key={index} item={item}/>
-        ));
-      }
-    return (
-        <div className='row'>
-            <ul>
-                {renderList()}
-            </ul>
-        </div>
-    )
+  const renderList = () => {
+    return items.map((item, index) => <Single key={index} item={item} />);
+  };
+  return (
+    <div className="row">
+      <ul>{renderList()}</ul>
+    </div>
+  );
 }
 
-export default Grid
+export default Grid;
