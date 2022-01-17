bcc86329d02238a90aba4f643fb46a533cc4a36a
daniel.velazquez <danicond97@gmail.com>
2022-01-17 03:01:30 -0600

    fix: eslint code fix

--- src/components/List/Grid.test.js
+++ src/components/List/Grid.test.js
@@ -1,21 +1,23 @@
-import Grid from "./Grid";
-import React from "react";
-import { screen, render } from '@testing-library/react';
+import Grid from './Grid';
+import React from 'react';
+import { render } from '@testing-library/react';
 
 import MockData from '../../data/youtube-videos-mock.json';
 
 describe('Testing the component elements', () => {
   test('Video preview title is displayed', () => {
-    const { getAllByText } = render(<Grid items={ MockData.items }/>)
+    const { getAllByText } = render(<Grid items={MockData.items} />);
     const title = getAllByText('Wizeline')[0];
     expect(title).toBeInTheDocument();
     // debug()
   });
   test('Video preview thumbnail is displayed', () => {
-    const { getAllByRole } = render(<Grid items={ MockData.items }/>)
+    const { getAllByRole } = render(<Grid items={MockData.items} />);
 
     const thumbnail = getAllByRole('img')[0];
-    expect(thumbnail.src).toMatch('https://yt3.ggpht.com/ytc/AAUvwnighSReQlmHl_S_vSfvnWBAG5Cw4A0YxtE0tm5OpQ=s800-c-k-c0xffffffff-no-rj-mo');
+    expect(thumbnail.src).toMatch(
+      'https://yt3.ggpht.com/ytc/AAUvwnighSReQlmHl_S_vSfvnWBAG5Cw4A0YxtE0tm5OpQ=s800-c-k-c0xffffffff-no-rj-mo'
+    );
   });
   // test('Number of items to be greater than or equal to 25', () => {
   //   expect(numItems).toBeGreaterThanOrEqual(25);
